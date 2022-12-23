8e01244cf03ed3c78e6453e939074d5d59ea1266
Bingo Babus <richard.bankemper@gmail.com>
2022-12-23 17:48:42 -0500

    Create MadnessMember.py

---
+++ Models/MadnessMember.py
@@ -0,0 +1,17 @@
+import discord
+
+class MadnessMember:
+    MythicMember = discord.Member
+    HasRole = False
+    Roles = ""
+
+    # Constractor of the class
+    def __init__(self, member: discord.Member, HasRole: bool, Roles: str):
+        self.MythicMember = member
+        self.HasRole = HasRole
+        self.Roles = Roles
+
+    def ToString(self):
+        return f"{self.MythicMember.mention}[{' '.join([m for m in self.Roles])}]"
+
+    
